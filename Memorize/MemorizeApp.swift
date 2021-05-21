89e3a02aac75e715d0c32e744f43b6d2ca3dd796
Chris Conner <chris.conner57@gmail.com>
2021-05-20 17:13:44 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chris Conner on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
